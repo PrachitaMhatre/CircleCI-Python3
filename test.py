84ad068844372b9c4c6dbd08813f30114535a7d5
Prachita Mhatre <prachitamhatre2000@gmail.com>
2023-09-10 17:44:20 +0530

    modified config.yml file by Prachita

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ class MyTestCase(unittest.TestCase):
     def test_to_uppper(self):
         name = "Prachita"
         upper_name = to_upper(name)
-        self.assertEqual(upper_name,"Prachita")
+        self.assertEqual(upper_name,"PRACHITA")
         
 if __name__ == '__main__':
     unittest.main()
